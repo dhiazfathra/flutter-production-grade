37fc8baf02cb6cbacf7be927112d869967e03590
Dhiaz Fathra <dhiazfathra@gmail.com>
2026-09-14 00:21:04 +0700

    style: format result.dart

--- lib/core/utils/result.dart
+++ lib/core/utils/result.dart
@@ -12,21 +12,20 @@ sealed class Result<T> with _$Result<T> {
   const factory Result.err(AppError error) = Err<T>;
 
   T? get valueOrNull => switch (this) {
-        Ok<T>(:final value) => value,
-        Err<T>() => null,
-      };
+    Ok<T>(:final value) => value,
+    Err<T>() => null,
+  };
 
   AppError? get errorOrNull => switch (this) {
-        Ok<T>() => null,
-        Err<T>(:final error) => error,
-      };
+    Ok<T>() => null,
+    Err<T>(:final error) => error,
+  };
 
   R fold<R>({
     required R Function(T value) ok,
     required R Function(AppError error) err,
-  }) =>
-      switch (this) {
-        Ok<T>(value: final v) => ok(v),
-        Err<T>(error: final e) => err(e),
-      };
+  }) => switch (this) {
+    Ok<T>(value: final v) => ok(v),
+    Err<T>(error: final e) => err(e),
+  };
 }
